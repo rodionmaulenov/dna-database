cf83513f7442211d52d86d55659232f34fd8b8d4
rodionmaulenov <rodion.maulenov@gmail.com>
2025-11-09 05:11:53 +0200

    change frontend dna-table height to 630px

--- frontend/src/app/upload/upload.service.ts
+++ frontend/src/app/upload/upload.service.ts
@@ -12,26 +12,6 @@ export class UploadService {
   private env = inject(ENVIRONMENT);
   private apiUrl = this.env.apiUrl;
 
-  // uploadFile(file: File): Observable<FileUploadResponse> {
-  //   const formData = new FormData();
-  //   formData.append('file', file);
-  //
-  //   return this.http.post<FileUploadResponse>(
-  //     `${this.apiUrl}/upload/file/`,
-  //     formData
-  //   );
-  // }
-  //
-  // matchFile(file: File, role: string): Observable<FileUploadResponse> {
-  //   const formData = new FormData();
-  //   formData.append('file', file);
-  //   formData.append('role', role);
-  //
-  //   return this.http.post<FileUploadResponse>(
-  //     `${this.apiUrl}/upload/match/`,
-  //     formData
-  //   );
-  // }
   uploadFile(file: File): Observable<FileUploadResponse> {
     const formData = new FormData();
     formData.append('file', file);
